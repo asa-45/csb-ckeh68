fb48dd906699c121bfaba6b51025e452a4d8576d
Code Sandbox <hello@codesandbox.io>
2023-02-20 12:57:33 +0000

    Deployment from Codesandbox.

--- precache-manifest.d5e97b15798e2d011702cb368d4afc93.js
+++ precache-manifest.d5e97b15798e2d011702cb368d4afc93.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "13bdab9f66b4485fe96a5658e3e77c77",
+    "revision": "b9c0ccc47d59b8402c6cdf2c828a0389",
     "url": "https://asa-45.github.io/csb-ckeh68/index.html"
   },
   {
-    "revision": "99f3b294099fd95b098a",
+    "revision": "1c7e6ea9dde0991313a9",
     "url": "https://asa-45.github.io/csb-ckeh68/static/css/main.96f7de54.chunk.css"
   },
   {
-    "revision": "dbddb86b7229907788b9",
-    "url": "https://asa-45.github.io/csb-ckeh68/static/js/2.bf3aa08b.chunk.js"
+    "revision": "387d1576ffc1ef940f2c",
+    "url": "https://asa-45.github.io/csb-ckeh68/static/js/2.3301b7f0.chunk.js"
   },
   {
-    "revision": "99f3b294099fd95b098a",
-    "url": "https://asa-45.github.io/csb-ckeh68/static/js/main.daf1296c.chunk.js"
+    "revision": "1c7e6ea9dde0991313a9",
+    "url": "https://asa-45.github.io/csb-ckeh68/static/js/main.32425632.chunk.js"
   },
   {
     "revision": "42b02062f3a577a94518",
